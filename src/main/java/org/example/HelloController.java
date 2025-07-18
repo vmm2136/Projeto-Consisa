43ba56aa00ef803dd78a5c4cc77a28e6393706f6
Victor Mariani <dev13@nodetech.com.br>
2025-07-18 00:19:05 -0300

    primeiro commit - testes

---
+++ src/main/java/org/example/HelloController.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class HelloController {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String digaOla(){
+        return  "Olá WildFly!";
+    }
+
+}
